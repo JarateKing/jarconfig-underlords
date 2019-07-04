5e68b8adf3b998e0a5bfd9e75770815382b29af4
JarateKing <scmacintosh@upei.ca>
2019-07-03 22:47:18 -0300

    added comments to spy

--- console_spy.py
+++ console_spy.py
@@ -1,16 +1,20 @@
 import time
 
+# keep track of the previous log
 oldlog = ""
 
 while True:
 	try:
+		# read the console log
 		f = open("../../console.log", "r")
 		newlog = f.read()
 		if oldlog != newlog:
+			# print any changes to it
 			print(newlog.replace(oldlog, ""), end="", flush=True)
 			oldlog = newlog
 		f.close()
 	except:
+		# error message otherwise
 		print("error reading file, retrying")
 		pass
 	time.sleep(1)
